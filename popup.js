8ed08dff78335e49516b6ad525c5b4eefaad684f
Abrahm241220 <120452259+Abrahm241220@users.noreply.github.com>
2022-12-13 03:19:43 -0500

    initial commit

---
+++ popup.js
@@ -0,0 +1,32 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('SABIA QUE DIRIAS SI')
+    alert('ME HACES LA PERSONA MAS FELIZ')
+    alert('Si escogías que no pues no podrás y te me aguantas y Gracias por aceptar no sabes cuanto te amo <3 no te arrepentirás')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+});
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
